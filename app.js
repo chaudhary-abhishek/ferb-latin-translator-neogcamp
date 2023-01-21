af7d6fd00db773e17146279dce2e508ec91be215
Abhishek Choudhary <abhishekch115@gmail.com>
2023-01-21 07:22:35 +0530

    added error handler

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ var txtOutput = document.querySelector("#txt-translated-output");
 
 var url = "https://api.funtranslations.com/translate/ferb-latin.json";
 
+function errorHandler(error){
+    console.log("There is some error "+error);
+    alert("something went wrong with server");
+}
+
 function urlGenerator(text){
     return url+"?text="+text;
 }
 
 function onClick(){
     var userInput = userTextElement.value;
-    fetch(urlGenerator(userInput)).then(response=> response.json()).then(json=>txtOutput.innerText=json.contents.translated);
+    fetch(urlGenerator(userInput)).then(response=> response.json()).then(json=>txtOutput.innerText=json.contents.translated).catch(errorHandler)
 
 }
 //console.log(translateBtn);
